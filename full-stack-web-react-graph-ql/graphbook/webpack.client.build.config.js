7bc6e85c3870b823548b84bc267b88ce49da22de
Bruno Santos <bruno.js02@gmail.com>
2020-03-31 17:36:14 -0300

    Added production build script

---
+++ full-stack-web-react-graph-ql/graphbook/webpack.client.build.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const buildDirectory = "dist";
+const outputDirectory = buildDirectory + "/client";
+
+module.exports = {
+  mode: "production",
+  entry: "./src/client/index.js",
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../"
+            }
+          },
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
+  ]
+};
